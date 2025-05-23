7e51dbcbffaedc27ea6069264153da038b4dcf01
Morozov M <m.a.m2003@mail.ru>
2025-05-24 01:09:08 +0300

    документация

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
 import './scss/styles.scss';
+// Используем паттерн Dependency Injection для более гибкого кода
+// Используем паттерн Controller для корневого класса - чтобы управлять всем приложением из одного класса
+
+/**
+ * Это класс в отдельном файле
+ */
+// class ComponentC {
+//     constructor(componentA, componentB, serviceA) {
+//         this.componentA = componentA;
+//         this.componentB = componentB;
+//         this.serviceA = serviceA;
+//     }
+// }
+
+/**
+ * Этот класс в отдельном файле
+ */
+class AppController {
+    // apiService = new Api();
+    // eventEmitter = new EventEmitter();
+    // componentA = new ComponentA();
+    // componentB = new ComponentB();
+    // serviceA = new ServiceA(this.apiService, this.eventEmitter);
+    // componentC = new ComponentC(this.componentA, this.componentB, this.serviceA);
+
+    constructor() {
+        this._loadAndRenderPosts();
+    }
+
+    private _loadAndRenderPosts(): void {}
+}
+
+const app = new AppController();
---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface IProductItem {
+  id: string,
+  name: string,
+  description: string,
+  category: string,
+  img: string,
+  price: number | null,
+}
+
+export interface IOrder {
+payment: string,
+address: string,
+phone: string,
+email: string,
+total: string | number,
+items: string[],
+}
+
+export interface IOrderResult {
+  id: string,    
+  total: number,
+}
